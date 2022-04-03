79e78d9e3651be930cca5b98733e425d39516d00
Vaalarivan2002 <pvaalarivan@gmail.com>
2022-04-03 11:09:34 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { isCursorAtStart } from '@testing-library/user-event/dist/utils';
 
 function App() {
+  let date = new Date()
+  let newDate = localStorage.getItem('date') != date.getDate()
+  if (newDate) {
+    localStorage.removeItem('s1')
+    localStorage.removeItem('s2')
+    localStorage.setItem('date', date.getDate())
+  }
+  
+  const [s1, setS1] = useState((localStorage.getItem('s1') !== null && localStorage.getItem('s1') !== "") ? "Today's timetable updated" : "")
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => {
+        localStorage.setItem('s1', "Today's timetable updated")
+        setS1("Today's timetable updated")
+      }}>Updated timetable</button>
+      <h1>{ s1 }</h1>
+      
     </div>
   );
 }
 
 export default App;
+
